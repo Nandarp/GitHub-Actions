46aa7c4b31518c98d518bdc0b047a32dd56d7aae
Nandarp <108810772+Nandarp@users.noreply.github.com>
2024-02-26 17:23:34 +0530

    Create format-html.js

---
+++ format-html.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const prettier = require('prettier');
+
+// Read raw HTML data
+const rawData = fs.readFileSync('apidog_report.html', 'utf8');
+
+// Format HTML using prettier
+const formattedHtml = prettier.format(rawData, {
+  parser: 'html',
+  printWidth: 120, // Adjust print width as needed
+});
+
+// Write formatted HTML to a new file
+fs.writeFileSync('pretty.html', formattedHtml);
+
+console.log('HTML formatting complete!');
