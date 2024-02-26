8facce26b48f6836dc8974849e99d497dbd3bc49
Nandarp <108810772+Nandarp@users.noreply.github.com>
2024-02-26 17:38:44 +0530

    Update format-html.js

--- format-html.js
+++ format-html.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const prettier = require('prettier');
 
-// Read raw HTML data
-const rawData = fs.readFileSync('testArtifacts/apidog_report.html', 'utf8');
+async function formatHtml() {
+  try {
+    // Read raw HTML data
+    const rawData = await fs.readFile('testArtifacts/apidog_report.html', 'utf8');
 
-// Format HTML using prettier
-const formattedHtml = prettier.format(rawData, {
-  parser: 'html',
-  printWidth: 120, // Adjust print width as needed
-});
+    // Format HTML using prettier
+    const formattedHtml = prettier.format(rawData, {
+      parser: 'html',
+      printWidth: 120, // Adjust print width as needed
+    });
 
-// Write formatted HTML to a new file
-fs.writeFileSync('pretty.html', formattedHtml);
+    // Write formatted HTML to a new file
+    await fs.writeFile('pretty.html', formattedHtml);
 
-console.log('HTML formatting complete!');
+    console.log('HTML formatting complete!');
+  } catch (error) {
+    console.error('Error formatting HTML:', error);
+    process.exit(1); // Exit with error status
+  }
+}
+
+formatHtml();
