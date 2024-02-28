e9b4b209203e077d4b871f7a56cccc63eb086779
Nandarp <108810772+Nandarp@users.noreply.github.com>
2024-02-28 17:04:52 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -6,7 +6,10 @@ const msg = {
   to: 'nandakumarap@anthology.com', // Change this to the recipient's email address
   from: 'ivpnotifications@products.anthology.com', // Change this to your verified sender email in SendGrid
   subject: 'APIdog Test Report',
-  text: 'APIdog test report is attached.',
+  text: 'Hi, Please find the attached APIdog test report.
+          
+          Thanks,
+          GitHub Actions',
   attachments: [
     {
       content: htmlReport,
